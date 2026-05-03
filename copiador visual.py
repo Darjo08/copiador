07c73a903121a411bea4d7fed19d811b7e3feeb9
Armando Villa <tecnicoupa11@gmail.com>
2026-05-03 00:06:23 +0000

    2 may 2026, 19:06

--- copiador visual.py
+++ copiador visual.py
@@ -64,7 +64,7 @@ class WorkerCopia(QThread):
     def __init__(self, directorios_origen, directorio_destino, copiar_todo, opciones_copia,
                   texto_reemplazo_rips, texto_sufijo_rips, texto_reemplazo_docker, texto_sufijo_docker,
                     copiar_en_raiz, comprimir_zip, aut_compensar, 
-                    texto_reemplazo_zip, texto_sufijo_zip, texto_reemplazo_directorios, texto_sufijo_directorios,texto_reemplazo_ad, texto_sufijo_ad):
+                    texto_reemplazo_zip, texto_sufijo_zip, texto_reemplazo_directorios, texto_sufijo_directorios, texto_reemplazo_ad, texto_sufijo_ad):
         super().__init__()
         self.directorios_origen = directorios_origen
         self.directorio_destino = directorio_destino
