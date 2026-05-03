6d56747b1975be04409e280db633970f87460e41
Armando Villa <tecnicoupa11@gmail.com>
2026-05-03 00:11:53 +0000

    2 may 2026, 19:11

--- copiador visual.py
+++ copiador visual.py
@@ -64,7 +64,7 @@ class WorkerCopia(QThread):
     def __init__(self, directorios_origen, directorio_destino, copiar_todo, opciones_copia,
                   texto_reemplazo_rips, texto_sufijo_rips, texto_reemplazo_docker, texto_sufijo_docker,
                     copiar_en_raiz, comprimir_zip, aut_compensar, 
-                    texto_reemplazo_zip, texto_sufijo_zip, texto_reemplazo_directorios, texto_sufijo_directorios, texto_reemplazo_ad, texto_sufijo_ad):
+                    texto_reemplazo_zip, texto_sufijo_zip, texto_reemplazo_directorios, texto_sufijo_directorios, texto_reemplazo_ad, texto_sufijo_ad, descargar_cuv):
         super().__init__()
         self.directorios_origen = directorios_origen
         self.directorio_destino = directorio_destino
@@ -83,6 +83,7 @@ class WorkerCopia(QThread):
         self.texto_sufijo_directorios = texto_sufijo_directorios
         self.texto_reemplazo_ad = texto_reemplazo_ad
         self.texto_sufijo_ad = texto_sufijo_ad
+        self.descargar_cuv = descargar_cuv
         self.formato_json = opciones_copia.get('formato_json', False)
         self.cancelar_flag = False
         self.archivos_copiados = 0
